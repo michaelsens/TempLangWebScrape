c30abdd7255ed3cc966326312d6d82111426c3d4
Michael Sensale <michaelrobert.sensale@gmail.com>
2022-06-24 13:20:36 -0400

    added temporary indent formatting

--- webscraper.py
+++ webscraper.py
@@ -12,9 +12,9 @@ def scrape_page(language_group):
     src = result.content
     soup = BeautifulSoup(src, 'lxml')
     groups = soup.find("div", {"class": "view-content indent1"})
-    recursive_search(groups.find("div", {"class": "item-list"}))
+    recursive_search(groups.find("div", {"class": "item-list"}), "")
 
-def recursive_search(item_list):
+def recursive_search(item_list, temp_indent):
     if item_list == None:
         return
     list = item_list.find("ul").find_all("li", recursive = False)
@@ -24,8 +24,8 @@ def recursive_search(item_list):
         if leaf_ul != []: 
             leaf_list = leaf_ul[0].find_all("li", recursive = False)
             for leaf_lang in leaf_list:
-                print(leaf_lang.find("span", {"class": "field-content"}).text)
-        print(lang.find('a').string)
-        recursive_search(lang.find("div", {"class": "item-list"}, recursive = False))
+                print(temp_indent + leaf_lang.find("span", {"class": "field-content"}).text)
+        print(temp_indent + lang.find('a').string)
+        recursive_search(lang.find("div", {"class": "item-list"}, recursive = False), temp_indent + "    ")
 
 scrape_page("afro-asiatic")
